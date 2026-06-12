cc90e82494e38faab5d9e67bc2e65a30f5a5bc8d
sota2006 <rakis1792@gmail.com>
2026-06-12 11:41:30 +0900

    1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import random
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -12,3 +12,18 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>私のホームページ</title>
+        </head>
+        <body>
+            <h1>ようこそ！</h1>
+            <p>これは私のページです。</p>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
